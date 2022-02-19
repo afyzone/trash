e39d06662f5df5df591478b23b46a938c4b41929
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-18 19:11:26 -0800

    Update OrbsofMagic.lua

--- OrbsofMagic.lua
+++ OrbsofMagic.lua
@@ -1,8 +1,10 @@
 pcall(function()
     while wait() do
-        if game:GetService("Workspace"):FindFirstChild("Time") then
-            fireclickdetector(game:GetService("Workspace").Time.Part.ClickDetector)
-            game:GetService("ReplicatedStorage").ServerEvents.Magic.GT:FireServer()
+        game:GetService("ReplicatedStorage").ServerEvents.Magic.GT:FireServer()
+        for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
+            if v:IsA("Part") and v.Parent.Name == Destruction and v.Parent.Name == Frost and v.Parent.Name == Lava and v.Parent.Name == Lunar and v.Parent.Name == Solar and v.Parent.Name == Acceleration and v.Parent.Name == Dragon and v.Parent.Name == Hydra and v.Parent.Name == Illusion and v.Parent.Name == Time and v.Parent.Name == Valkyrie then
+                fireclickdetector(v.ClickDetector)
+            end
         end
     end
 end)
