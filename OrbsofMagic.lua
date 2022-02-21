323af5bf8cf356c7ca0ad80b037fdf2af9d1f493
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-21 11:15:37 -0800

    Update OrbsofMagic.lua

--- OrbsofMagic.lua
+++ OrbsofMagic.lua
@@ -2,7 +2,7 @@ pcall(function()
     while wait() do
         game:GetService("ReplicatedStorage").ServerEvents.Magic.GT:FireServer()
         for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
-            if v:IsA("Part") and v.Parent.Name == Destruction and v.Parent.Name == Frost and v.Parent.Name == Lava and v.Parent.Name == Lunar and v.Parent.Name == Solar and v.Parent.Name == Acceleration and v.Parent.Name == Dragon and v.Parent.Name == Hydra and v.Parent.Name == Illusion and v.Parent.Name == Time and v.Parent.Name == Valkyrie then
+            if v:IsA("Part") and v.Parent.Name == Reaper and v.Parent.Name == Time and v.Parent.Name == Illusion and v.Parent.Name == Raven and v.Parent.Name == Jester and v.Parent.Name == Acceleration and v.Parent.Name == Phoenix then
                 fireclickdetector(v.ClickDetector)
                 wait(5)
                 game.Players.LocalPlayer:Kick("You got your orb")
